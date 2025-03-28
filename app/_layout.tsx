9d040813a642930681c8757a574188af8a19a06e
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-03-28 17:00:39 +0530

    Customizing navigation header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,38 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { BackHandler } from "react-native";
+import { useNavigation } from "expo-router";
 
 export default function Layout() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // Disable Android Hardware Back Button
+    const preventBack = () => true;
+    BackHandler.addEventListener("hardwareBackPress", preventBack);
+
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", preventBack);
+    };
+  }, []);
+
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerTitleAlign: "center",
+        headerStyle: { backgroundColor: "#f8f8f8" },
+        headerTintColor: "#333",
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
       <Stack.Screen name="Splash/index" options={{ headerShown: false }} />
       <Stack.Screen
         name="Home/index"
-        options={{ title: "Home"}}
+        options={{
+          title: "Home",
+          headerBackVisible: false,
+          gestureEnabled: false, // Disable swipe back on iOS
+        }}
       />
     </Stack>
   );

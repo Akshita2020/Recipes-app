8a2c1b0f0696cea0017f89af1e1ad408ccaceeaf
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-03 18:00:49 +0530

    Showing nutritions data

--- app/RecipeDetails/index.tsx
+++ app/RecipeDetails/index.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
 import styles from "./styles";
 import Title from "@/components/Title";
 
 const RecipeDetails = ({ route }) => {
   const { item } = route?.params || {};
+  const nutrition = item?.nutrition;
+  const nutritionKeys = Object.keys(nutrition || {});
+  console.log(nutrition);
+  console.log(nutritionKeys);
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={{ uri: item?.thumbnail_url }} />
       <Title text={item?.name} />
+      {nutritionKeys?.map(key=>(
+        <View key={key} style={styles.row}>
+          <Text style={styles.key}>{key}</Text>
+          <Text style={styles.value}>{nutrition[key]}</Text>
+          
+          </View>
+      ))}
     </View>
   );
 };
--- app/RecipeDetails/styles.tsx
+++ app/RecipeDetails/styles.tsx
@@ -1,3 +1,4 @@
+import colors from "@/constants/colors";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
@@ -5,12 +6,31 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     flex: 1,
   },
-  image:{
-    width: '100%',
+  image: {
+    width: "100%",
     height: 180,
-    borderRadius:10,
-    marginBottom:12
-  }
+    borderRadius: 10,
+    marginBottom: 12,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "rgba(217,217,217,0.5)",
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+    marginVertical: 4,
+  },
+  key: {
+    fontSize: 12,
+    color: colors.black,
+    textTransform: "capitalize",
+  },
+  value: {
+    fontSize: 12,
+    color: colors.lightGrey2,
+  },
 });
 
 export default styles;
